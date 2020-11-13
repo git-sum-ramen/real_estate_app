c6f480133de2bcf407af377271d065d990b79c72
brian <riceb53@gmail.com>
2020-11-12 21:48:42 -0500

    listings create

---
+++ app/controllers/api/listings_controller.rb
@@ -0,0 +1,18 @@
+class Api::ListingsController < ApplicationController
+  def create
+    @listing = Listing.new(
+      description: params[:description],
+      year_built: params[:year_built],
+      square_feet: params[:square_feet],
+      bedrooms: params[:bedrooms],
+      bathrooms: params[:bathrooms],
+      floors: params[:floors],
+      availability: params[:availability],
+      price:  params[:price],
+      latitude: params[:latitude],
+      longitude: params[:longitude],
+    )
+    @listing.save
+    render 'show.json.jb'
+  end
+end

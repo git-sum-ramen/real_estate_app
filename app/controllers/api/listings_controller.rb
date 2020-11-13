9bcc9ff9d18c40cc3e454ba0b267837f017ba66d
brian <riceb53@gmail.com>
2020-11-12 22:00:51 -0500

    add geocoder for dynamic lat lng

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,5 +1,10 @@
 class Api::ListingsController < ApplicationController
   def create
+    results = Geocoder.search(params[:address])
+    the_latitude = results.first.coordinates[0]
+    the_longitude = results.first.coordinates[1]
+    p the_latitude
+
     @listing = Listing.new(
       description: params[:description],
       year_built: params[:year_built],
@@ -9,8 +14,8 @@ class Api::ListingsController < ApplicationController
       floors: params[:floors],
       availability: params[:availability],
       price:  params[:price],
-      latitude: params[:latitude],
-      longitude: params[:longitude],
+      latitude: the_latitude,
+      longitude: the_longitude,
     )
     @listing.save
     render 'show.json.jb'
